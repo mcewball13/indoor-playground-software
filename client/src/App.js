3e88a4cbe4131044c63d0e7df90ad1214d796bc3
Mike <mcewen1984@hotmail.com>
2022-01-13 20:21:36 -0800

    added front end elements from MUI

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,12 @@ import ThemeProvider from './theme';
 // components
 import Settings from './components/settings';
 import RtlLayout from './components/RtlLayout';
+import { ChartStyle } from './components/chart';
 import ScrollToTop from './components/ScrollToTop';
 import { ProgressBarStyle } from './components/ProgressBar';
+import NotistackProvider from './components/NotistackProvider';
 import ThemeColorPresets from './components/ThemeColorPresets';
+import ThemeLocalization from './components/ThemeLocalization';
 import MotionLazyContainer from './components/animate/MotionLazyContainer';
 
 // ----------------------------------------------------------------------
@@ -16,15 +19,19 @@ export default function App() {
   return (
     <ThemeProvider>
       <ThemeColorPresets>
-        <RtlLayout>
-          <MotionLazyContainer>
-            <ProgressBarStyle />
-
-            <Settings />
-            <ScrollToTop />
-            <Router />
-          </MotionLazyContainer>
-        </RtlLayout>
+        <ThemeLocalization>
+          <RtlLayout>
+            <NotistackProvider>
+              <MotionLazyContainer>
+                <ProgressBarStyle />
+                <ChartStyle />
+                <Settings />
+                <ScrollToTop />
+                <Router />
+              </MotionLazyContainer>
+            </NotistackProvider>
+          </RtlLayout>
+        </ThemeLocalization>
       </ThemeColorPresets>
     </ThemeProvider>
   );
