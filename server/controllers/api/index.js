a15247c59803e5ed8f507b94d6114fc98769386e
Mike <mcewen1984@hotmail.com>
2022-01-25 07:53:47 -0800

    added get route for roles

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const customerRoutes = require('./customerRoutes');
+const employeeRoutes = require('./employeeRoutes');
 
 
-router.use('/customers', customerRoutes);
+router.use('/employees', employeeRoutes);
 
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser())
+app.use(routes)
 
 
 if (process.env.NODE_ENV === 'production') {
