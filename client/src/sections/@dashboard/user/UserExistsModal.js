7c70f7cd05d478197f0f177e8f695f4cb4d52081
mcewball13 <mcewen1984@hotmail.com>
2022-03-28 19:07:35 -0700

    removed rerender from modal causing bug. put form inside dialogue animate

--- client/src/sections/@dashboard/user/UserExistsModal.js
+++ client/src/sections/@dashboard/user/UserExistsModal.js
@@ -1,77 +1,110 @@
-import React, {useState} from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
 
 // mui
-import { Box,
-    Grid,
-    Typography,
-    InputAdornment,
-    IconButton,
-    DialogActions,} from "@mui/material"
+import {
+  Box,
+  Grid,
+  Typography,
+  InputAdornment,
+  IconButton,
+  DialogActions,
+  TextField,
+  Stack,
+  Button,
+} from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+
+// form
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, Controller } from 'react-hook-form';
 
 // componenets
 import { DialogAnimate } from '../../../components/animate';
-import { RHFTextField } from '../../../components/hook-form';
+import { FormProvider, RHFTextField } from '../../../components/hook-form';
 import Iconify from '../../../components/Iconify';
 
+const UserExistsModal = ({ isOpen, onClose, muiWidth, email }) => {
+  // state for show password
+  const [showPassword, setShowPassword] = useState(false);
 
+  const UserExistsModal = Yup.object().shape({
+    modalPassword: Yup.string().required('Password is required'),
+  });
 
-const UserExistsModal = ({isOpen, onClose, muiWidth, email}) => {
-// state for show password
-const [showPassword, setShowPassword] = useState(false);
+  // .test('required', 'Avatar is required', (value) => value !== ''),
 
+  const defaultValues = useMemo(
+    () => ({
+      modalPassword: '',
+    }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(UserExistsModal),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <DialogAnimate maxWidthMUI={muiWidth} open={isOpen} onClose={onClose} >
-        <DialogActions sx={{ py: 2, px: 3 }}>
-          <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
-            Existing Customer Found
-          </Typography>
-        </DialogActions>
-        <Box p={2}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <Typography variant="p">
-                An account with <strong>{email || 'this email address'}</strong> already exists. You can load you
-                account now by typing in your password. If you wish to use another email address, you can just close
-                this dialogue.
-              </Typography>
-            </Grid>
-            <Grid justifyContent={'space-between'} container item xs={12} spacing={2}>
-              <Grid item xs={6}>
-                <RHFTextField
-                  name="password"
-                  label="Password"
-                  type={showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                          <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <LoadingButton type="submit" variant="contained">
-                  Load Account
-                </LoadingButton>
-              </Grid>
-            </Grid>
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3} sx={{ p: 3 }}>
+        <Typography variant="p">
+          An account with <strong>{email || 'this email address'}</strong> already exists. You can load you account now
+          by typing in your password. If you wish to use another email address, you can just close this dialogue.
+        </Typography>
+
+        <RHFTextField
+          name="modalPassword"
+          label="Password"
+          type={showPassword ? 'text' : 'password'}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+
+        <Grid container gap={3}>
+          <Grid item>
+            <LoadingButton type="submit" variant="contained">
+              Load Account
+            </LoadingButton>
           </Grid>
-        </Box>
-      </DialogAnimate>
-  )
-}
+          <Grid item>
+            <Button variant="outlined" color="inherit" onClick={onClose}>
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </Stack>
+    </FormProvider>
+  );
+};
 
 UserExistsModal.propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    onClose: PropTypes.func.isRequired,
-    muiWidth: PropTypes.string.isRequired,
-    email: PropTypes.string,
-}
+  email: PropTypes.string,
+};
 
-export default UserExistsModal
+export default UserExistsModal;
--- client/src/sections/@dashboard/user/UserWaiverForm.js
+++ client/src/sections/@dashboard/user/UserWaiverForm.js
@@ -26,6 +26,8 @@ import {
   Table,
   TableRow,
   TableCell,
+  DialogTitle,
+  DialogActions,
 } from '@mui/material';
 // utils
 import { format } from 'date-fns';
@@ -42,6 +44,7 @@ import RHFDatePicker from '../../../components/hook-form/RHFDatePicker';
 import { RHFChooseAvatar } from '../../../components/hook-form/RHFChooseAvatar';
 import useAuth from '../../../hooks/useAuth';
 import { AuthContext } from '../../../contexts/JWTContext';
+import { DialogAnimate } from '../../../components/animate';
 import UserExistsModal from './UserExistsModal';
 
 // ----------------------------------------------------------------------
@@ -211,76 +214,77 @@ export default function UserWaiverForm({ isEdit, currentUser, isOpen, onOpen, on
   const formatDate = (date) => format(date, 'MM/dd/yyyy');
 
   return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Grid container spacing={3}>
-        <Grid item container xs={12} md={4} spacing={3}>
-          {/* Avatar upload plate */}
-          <Grid item xs={12}>
-            <Card sx={{ py: 10, px: 3 }}>
-              {isEdit && (
-                <Label
-                  color={values.status !== 'active' ? 'error' : 'success'}
-                  sx={{ textTransform: 'uppercase', position: 'absolute', top: 24, right: 24 }}
-                >
-                  {values.status}
-                </Label>
-              )}
+    <>
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={3}>
+          <Grid item container xs={12} md={4} spacing={3}>
+            {/* Avatar upload plate */}
+            <Grid item xs={12}>
+              <Card sx={{ py: 10, px: 3 }}>
+                {isEdit && (
+                  <Label
+                    color={values.status !== 'active' ? 'error' : 'success'}
+                    sx={{ textTransform: 'uppercase', position: 'absolute', top: 24, right: 24 }}
+                  >
+                    {values.status}
+                  </Label>
+                )}
 
-              <Box sx={{ mb: 5 }}>
-                <RHFChooseAvatar
-                  name="avatarUrl"
-                  accept="image/*"
-                  onDrop={handleDrop}
-                  helperText={
-                    <Typography
-                      variant="caption"
-                      sx={{
-                        mt: 2,
-                        mx: 'auto',
-                        display: 'block',
-                        textAlign: 'center',
-                        color: 'text.secondary',
-                      }}
-                    >
-                      If you don't choose an avatar your
-                      <br />
-                      initial will be used
-                    </Typography>
-                  }
-                />
-              </Box>
-
-              {isEdit && (
-                <FormControlLabel
-                  labelPlacement="start"
-                  control={
-                    <Controller
-                      name="status"
-                      control={control}
-                      render={({ field }) => (
-                        <Switch
-                          {...field}
-                          checked={field.value !== 'active'}
-                          onChange={(event) => field.onChange(event.target.checked ? 'banned' : 'active')}
-                        />
-                      )}
-                    />
-                  }
-                  label={
-                    <>
-                      <Typography variant="subtitle2" sx={{ mb: 0.5 }}>
-                        Banned
+                <Box sx={{ mb: 5 }}>
+                  <RHFChooseAvatar
+                    name="avatarUrl"
+                    accept="image/*"
+                    onDrop={handleDrop}
+                    helperText={
+                      <Typography
+                        variant="caption"
+                        sx={{
+                          mt: 2,
+                          mx: 'auto',
+                          display: 'block',
+                          textAlign: 'center',
+                          color: 'text.secondary',
+                        }}
+                      >
+                        If you don't choose an avatar your
+                        <br />
+                        initial will be used
                       </Typography>
-                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                        Apply disable account
-                      </Typography>
-                    </>
-                  }
-                  sx={{ mx: 0, mb: 3, width: 1, justifyContent: 'space-between' }}
-                />
-              )}
+                    }
+                  />
+                </Box>
 
-              {/* <RHFSwitch
+                {isEdit && (
+                  <FormControlLabel
+                    labelPlacement="start"
+                    control={
+                      <Controller
+                        name="status"
+                        control={control}
+                        render={({ field }) => (
+                          <Switch
+                            {...field}
+                            checked={field.value !== 'active'}
+                            onChange={(event) => field.onChange(event.target.checked ? 'banned' : 'active')}
+                          />
+                        )}
+                      />
+                    }
+                    label={
+                      <>
+                        <Typography variant="subtitle2" sx={{ mb: 0.5 }}>
+                          Banned
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                          Apply disable account
+                        </Typography>
+                      </>
+                    }
+                    sx={{ mx: 0, mb: 3, width: 1, justifyContent: 'space-between' }}
+                  />
+                )}
+
+                {/* <RHFSwitch
                 name="isBanned"
                 labelPlacement="start"
                 label={
@@ -295,143 +299,156 @@ export default function UserWaiverForm({ isEdit, currentUser, isOpen, onOpen, on
                 }
                 sx={{ mx: 0, width: 1, justifyContent: 'space-between' }}
               /> */}
-            </Card>
-          </Grid>
-        </Grid>
-
-        <Grid container item xs={12} md={8} gap={3}>
-          <Grid item xs={12}>
-            <Card sx={{ p: 3 }}>
-              <Box
-                sx={{
-                  display: 'grid',
-                  columnGap: 2,
-                  rowGap: 3,
-                  gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
-                }}
-              >
-                <RHFTextField onBlur={handleBlur} name="email" label="Email Address" />
-                <RHFTextField name="guardianFirstName" label="First Name" />
-                <RHFTextField name="guardianLastName" label="Last Name" />
-                <RHFDatePicker name="guardianBirthday" label="Guardian Birth Date" openTo="year" />
-                <RHFTextField
-                  name="password"
-                  label="Password"
-                  type={showPassword ? 'text' : 'password'}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                          <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <RHFTextField name="addressPhone" label="Phone Number" />
-                <RHFTextField name="addressStreet" label="Street Address" />
-                <RHFTextField name="addressCity" label="City" />
-                <RHFTextField name="addressState" label="State" />
-                <RHFTextField name="addressZipCode" label="Zip Code" />
-              </Box>
-
-              <Stack gap={3} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }} sx={{ mt: 3 }}>
-                <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-                  {!isEdit ? 'Sign Waiver' : 'Save Changes'}
-                </LoadingButton>
-
-                <Button size="small" startIcon={<Iconify icon={'eva:plus-fill'} />} onClick={onOpen} disabled={isOpen}>
-                  Add a Minor
-                </Button>
-              </Stack>
-
-              {/* Add Minor form */}
-              <Collapse
-                onEntered={() => handleOnEntered(addMinorFormScrollRef)}
-                in={isOpen}
-                ref={addMinorFormScrollRef}
-              >
-                <Box
-                  sx={{
-                    padding: 3,
-                    marginTop: 3,
-                    borderRadius: 1,
-                    bgcolor: 'background.paper',
-                  }}
-                >
-                  <Stack spacing={3}>
-                    <Typography variant="subtitle1" sx={{ color: 'text.secondary' }}>
-                      {' '}
-                      Add a Minor
-                    </Typography>
-
-                    <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-                      <RHFTextField name="minorFirstName" fullWidth label="First Name" />
-                      <RHFTextField name="minorLastName" fullWidth label="Last Name" />
-                    </Stack>
-                    <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-                      <RHFDatePicker name="minorBirthday" label="Birth Date" openTo="year" />
-                    </Stack>
-                    <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-                      <Button onClick={onCancel} color="error" startIcon={<Iconify icon={'eva:close-outline'} />}>
-                        Cancel
-                      </Button>
-                      <Button
-                        color="success"
-                        onClick={() =>
-                          handleSetMinors({
-                            minorFirstName: values.minorFirstName,
-                            minorLastName: values.minorLastName,
-                            minorBirthday: values.minorBirthday,
-                            email: values.email,
-                          })
-                        }
-                        startIcon={<Iconify icon={'eva:plus-fill'} />}
-                      >
-                        Add
-                      </Button>
-                    </Stack>
-                  </Stack>
-                </Box>
-              </Collapse>
-            </Card>
-          </Grid>
-          {/* Added minors list plate */}
-          {!!minors.length && (
-            <Grid item xs={12}>
-              <Card sx={{ py: 2, px: 1 }}>
-                <Typography variant="button" sx={{ mb: 1 }}>
-                  Added minors
-                </Typography>
-                <TableContainer>
-                  <Table>
-                    <TableBody>
-                      {minors.map((minor, i) => (
-                        <TableRow key={`${minor.minorFirstName}_${i}`} hover ref={addedMinorScrollRef}>
-                          <TableCell>
-                            <Typography variant="body1">{minor.minorFirstName}</Typography>
-                          </TableCell>
-                          <TableCell>
-                            <Typography variant="body1">{formatDate(minor.minorBirthday)}</Typography>
-                          </TableCell>
-                          <TableCell align="right">
-                            <UserMoreMenu
-                              isMinor
-                              userName={minor.minorFirstName}
-                              onDelete={() => handleRemoveMinor(minor.id)}
-                            />
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
               </Card>
             </Grid>
-          )}
+          </Grid>
+
+          <Grid container item xs={12} md={8} gap={3}>
+            <Grid item xs={12}>
+              <Card sx={{ p: 3 }}>
+                <Box
+                  sx={{
+                    display: 'grid',
+                    columnGap: 2,
+                    rowGap: 3,
+                    gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
+                  }}
+                >
+                  <RHFTextField onBlur={handleBlur} name="email" label="Email Address" />
+                  <RHFTextField name="guardianFirstName" label="First Name" />
+                  <RHFTextField name="guardianLastName" label="Last Name" />
+                  <RHFDatePicker name="guardianBirthday" label="Guardian Birth Date" openTo="year" />
+                  <RHFTextField
+                    name="password"
+                    label="Password"
+                    type={showPassword ? 'text' : 'password'}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                            <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                  <RHFTextField name="addressPhone" label="Phone Number" />
+                  <RHFTextField name="addressStreet" label="Street Address" />
+                  <RHFTextField name="addressCity" label="City" />
+                  <RHFTextField name="addressState" label="State" />
+                  <RHFTextField name="addressZipCode" label="Zip Code" />
+                </Box>
+
+                <Stack gap={3} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }} sx={{ mt: 3 }}>
+                  <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                    {!isEdit ? 'Sign Waiver' : 'Save Changes'}
+                  </LoadingButton>
+
+                  <Button
+                    size="small"
+                    startIcon={<Iconify icon={'eva:plus-fill'} />}
+                    onClick={onOpen}
+                    disabled={isOpen}
+                  >
+                    Add a Minor
+                  </Button>
+                </Stack>
+
+                {/* Add Minor form */}
+                <Collapse
+                  onEntered={() => handleOnEntered(addMinorFormScrollRef)}
+                  in={isOpen}
+                  ref={addMinorFormScrollRef}
+                >
+                  <Box
+                    sx={{
+                      padding: 3,
+                      marginTop: 3,
+                      borderRadius: 1,
+                      bgcolor: 'background.paper',
+                    }}
+                  >
+                    <Stack spacing={3}>
+                      <Typography variant="subtitle1" sx={{ color: 'text.secondary' }}>
+                        {' '}
+                        Add a Minor
+                      </Typography>
+
+                      <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                        <RHFTextField name="minorFirstName" fullWidth label="First Name" />
+                        <RHFTextField name="minorLastName" fullWidth label="Last Name" />
+                      </Stack>
+                      <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                        <RHFDatePicker name="minorBirthday" label="Birth Date" openTo="year" />
+                      </Stack>
+                      <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                        <Button onClick={onCancel} color="error" startIcon={<Iconify icon={'eva:close-outline'} />}>
+                          Cancel
+                        </Button>
+                        <Button
+                          color="success"
+                          onClick={() =>
+                            handleSetMinors({
+                              minorFirstName: values.minorFirstName,
+                              minorLastName: values.minorLastName,
+                              minorBirthday: values.minorBirthday,
+                              email: values.email,
+                            })
+                          }
+                          startIcon={<Iconify icon={'eva:plus-fill'} />}
+                        >
+                          Add
+                        </Button>
+                      </Stack>
+                    </Stack>
+                  </Box>
+                </Collapse>
+              </Card>
+            </Grid>
+            {/* Added minors list plate */}
+            {!!minors.length && (
+              <Grid item xs={12}>
+                <Card sx={{ py: 2, px: 1 }}>
+                  <Typography variant="button" sx={{ mb: 1 }}>
+                    Added minors
+                  </Typography>
+                  <TableContainer>
+                    <Table>
+                      <TableBody>
+                        {minors.map((minor, i) => (
+                          <TableRow key={`${minor.minorFirstName}_${i}`} hover ref={addedMinorScrollRef}>
+                            <TableCell>
+                              <Typography variant="body1">{minor.minorFirstName}</Typography>
+                            </TableCell>
+                            <TableCell>
+                              <Typography variant="body1">{formatDate(minor.minorBirthday)}</Typography>
+                            </TableCell>
+                            <TableCell align="right">
+                              <UserMoreMenu
+                                isMinor
+                                userName={minor.minorFirstName}
+                                onDelete={() => handleRemoveMinor(minor.id)}
+                              />
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Card>
+              </Grid>
+            )}
+          </Grid>
         </Grid>
-      </Grid>
-      <UserExistsModal muiWidth='sm' isOpen={isOpenModalCustomerExists} onClose={handleCustomerExistsCloseModal} email={values.email} />
-    </FormProvider>
+      </FormProvider>
+      <DialogAnimate maxWidthMUI="sm" open={isOpenModalCustomerExists} onClose={handleCustomerExistsCloseModal}>
+        <DialogActions sx={{ py: 2, px: 3 }}>
+          <Typography variant="subtitle1" sx={{ flexGrow: 1 }}>
+            Existing Customer Found
+          </Typography>
+        </DialogActions>
+        <UserExistsModal onClose={handleCustomerExistsCloseModal} email={values.email} />
+      </DialogAnimate>
+    </>
   );
 }
