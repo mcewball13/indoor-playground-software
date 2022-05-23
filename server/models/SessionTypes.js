cd0c0560f12c8f8b5af5317b6d85c4b99fcbc338
mcewball13 <mcewen1984@hotmail.com>
2022-05-23 16:33:00 -0700

    added session type model

---
+++ server/models/SessionTypes.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const moment = require("moment");
+
+const momentDate = moment().format("MMM Do YYYY")
+
+class SessionTypes extends Model {}
+
+SessionTypes.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        description: {
+            type: DataTypes.STRING(500),
+            allowNull: false,
+            validate: {
+                len: [1],
+            },
+        },
+        notes: {
+            type: DataTypes.STRING(1000),
+            allowNull: true,
+        },
+        expireType: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        isRenewable: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        renewalPrice: {
+            type: DataTypes.FLOAT,
+            allowNull: true,
+        },
+        minLengthInMonths: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: 1,
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "session_types",
+    }
+);
+
+module.exports = SessionTypes;
