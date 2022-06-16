371b7e9233ef238408ca1dab47aef9411d59a9e4
mcewball13 <mcewen1984@hotmail.com>
2022-06-16 06:31:49 -0700

    changing grib to stack in the cart component

--- client/src/sections/@dashboard/e-commerce/pos-shop/CartSidebar.js
+++ client/src/sections/@dashboard/e-commerce/pos-shop/CartSidebar.js
@@ -1,32 +1,34 @@
-import { Box, Card, Typography, Stack, Button } from '@mui/material';
+import { Grid, Card, Typography, Stack, Button } from '@mui/material';
 import { alpha, styled } from '@mui/material/styles';
 import React from 'react';
 
 // ----------------------------------------------------------------------
 
 const RootStyle = styled('div')(() => ({
-    flexGrow: 1,
-    height: '100%',
-    overflow: 'hidden',
-  }));
+  flexGrow: 1,
+  height: '100%',
+  overflow: 'hidden',
+  position: 'relative',
+  borderRadius: 8,
+}));
 
 export default function CartSidebar() {
-
   return (
-    <Card sx={{ position: 'relative', p: 1, bgcolor: 'grey.200', height: '100%' }}>
-      <Box sx={{ p: 2 }}>
+    <RootStyle sx={{ bgcolor: 'grey.200', p: 1 }}>
+      <Stack sx={{ p: 2 }} spacing={2}>
         <Typography variant="h6">Cart</Typography>
-      </Box>
-      <Stack spacing={2} direction="column">
+
         <Stack spacing={2} direction="column">
-          <Card sx={{ p: 1, borderRadius: 1 }}>
-            <Typography variant="body2">Cart item</Typography>
-          </Card>
+          <Stack spacing={2} direction="column">
+            <Card sx={{ p: 1, borderRadius: 1 }}>
+              <Typography variant="body2">Cart item</Typography>
+            </Card>
+          </Stack>
         </Stack>
         <Button variant="contained" color="primary" fullWidth>
           Collect
         </Button>
       </Stack>
-    </Card>
+    </RootStyle>
   );
 }
