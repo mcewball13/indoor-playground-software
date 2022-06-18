f8c7137bc0f839e5f138a69c1f847bf20a583f29
mcewball13 <mcewen1984@hotmail.com>
2022-06-18 14:05:33 -0700

    switched cart item to seperate file and map through mock arr

--- client/src/sections/@dashboard/e-commerce/pos-shop/CartSidebar.js
+++ client/src/sections/@dashboard/e-commerce/pos-shop/CartSidebar.js
@@ -2,7 +2,31 @@ import { Grid, Card, Typography, Stack, Button, IconButton } from '@mui/material
 import { alpha, styled } from '@mui/material/styles';
 import React from 'react';
 import Iconify from '../../../../components/Iconify';
+import CartItem from './CartItem';
 
+const cartItems = [
+  {
+    key: '1',
+    title: 'First Child',
+    isAdmission: true,
+    isWaiverSigned: false,
+    onDelete: () => {},
+  },
+  {
+    key: '2',
+    title: 'Sibling',
+    isAdmission: true,
+    isWaiverSigned: true,
+    onDelete: () => {},
+  },
+  {
+    key: '3',
+    title: 'Chocolate Milk',
+    isAdmission: false,
+    isWaiverSigned: null,
+    onDelete: () => {},
+  }
+];
 // ----------------------------------------------------------------------
 
 const RootStyle = styled('div')(() => ({
@@ -14,25 +38,24 @@ const RootStyle = styled('div')(() => ({
 }));
 
 export default function CartSidebar() {
+
+  const setBgColor = (isWaiverSigned) => {
+    if (isWaiverSigned) {
+      return 'success.lighter';
+    } else if (isWaiverSigned === false) {
+      return 'warning.lighter';
+    } else {
+      return 'white.main';
+    }
+  }
+
   return (
     <RootStyle sx={{ bgcolor: 'grey.200', p: 1 }}>
       <Stack sx={{ p: 1 }} spacing={2}>
         <Typography variant="h6">Cart</Typography>
-
         <Stack spacing={2} direction="column">
-          <Card sx={{ p: 2, borderRadius: 1 }}>
-            <Stack spacing={2} direction="row" alignItems="center">
-              <Typography flex={1} variant="body2">
-                Cart item
-              </Typography>
-              <Iconify icon="ic:round-view-day" width={20} height={20} />
-              <IconButton>
-                <Iconify icon={'eva:close-fill'} width={20} height={20} />
-              </IconButton>
-            </Stack>
-          </Card>
+         {cartItems.map(item => <CartItem key={item.key} bgColor={setBgColor(item.isWaiverSigned)} title={item.title} isAdmission={item.isAdmission} />)}
         </Stack>
-
         <Button variant="contained" color="primary" fullWidth>
           Collect
         </Button>
