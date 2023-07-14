00522e1186c3e36040dfa03b9cce6b1a738fb924
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-07-13 17:54:44 -0700

    fixed typing errors for typescript move over

--- __TS__Conversion/src/sections/waiver/view/waiver-sign-view.tsx
+++ __TS__Conversion/src/sections/waiver/view/waiver-sign-view.tsx
@@ -5,12 +5,12 @@ import { Button, Card, Grid, Stack, Typography } from '@mui/material';
 import Container from '@mui/material/Container'
 import { LoadingButton } from '@mui/lab';
 import { useSnackbar } from 'notistack';
-import { capitalCase } from  'change-case'; 
+import { capitalCase } from 'change-case';
 import { useEffect, useState, useRef, useMemo } from 'react';
 // import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import { useRouter } from 'src/routes/hook'
 import SignatureCanvas from 'react-signature-canvas';
-import { PDFExport} from '@progress/kendo-react-pdf';
+import { PDFExport } from '@progress/kendo-react-pdf';
 import { drawDOM, exportPDF } from '@progress/kendo-drawing';
 // modules
 import DOMPurify from 'isomorphic-dompurify';
@@ -30,7 +30,7 @@ import { waiverText } from '../tempWaiverText';
 // routes
 import { paths } from '../../../routes/paths';
 
-import  FormProvider  from 'src/components/hook-form';
+import FormProvider from 'src/components/hook-form';
 import HTMLBlock from './HTML-block-view';
 //import JwtRegisterView from '../../auth/jwt/jwt-register-view'
 import { current } from '@reduxjs/toolkit';
@@ -43,33 +43,56 @@ const safeHTML = DOMPurify.sanitize(waiverText);
 
 //temporary hard code of values
 const JwtRegisterView = {
-  customer: "Jim Halpert",
-  submitSignedWaiver: true,
+  customer: {
+    id: 2,
+    minors: [
+      {
+        id:1,
+       minorFirstName: "John",
+       minorLastName: "Cena",
+      },
+      {
+        id:2,
+        minorFirstName: "John",
+        minorLastName: "Cena",
+      }
+    ],
+    guardianFirstName: "Jim",
+    guardianLastName: "Halpert",
+  },
+  submitSignedWaiver(){
+
+  }
+}
+
+const minor = {
+  minorFirstName: "Cece",
+  minorLastName: "Halpert"
 }
 
 
 
 
 export default function SignWaiver() {
-  const {push} = useRouter();
+  const { push } = useRouter();
   //waiting on new auth
   const { customer, submitSignedWaiver } = JwtRegisterView;
   const settings = useSettingsContext();
-  const { enqueueSnackbar} = useSnackbar();
+  const { enqueueSnackbar } = useSnackbar();
   // const { id = '' } = query;
   // const isEdit = pathname.includes('edit');
-  const signatureRef = useRef<HTMLElement | null>(null);
-  const signatureBlockCardRef = useRef<HTMLElement | null>(null);
-  const pdfWaiverElement = useRef(null);
-  const pdfWaiverElementDownload = useRef(null);
-  
+  const signatureRef = useRef<SignatureCanvas | null>(null);
+  const signatureBlockCardRef = useRef<HTMLDivElement | null>(null);
+  const pdfWaiverElement = useRef<HTMLDivElement | null>(null);
+  const pdfWaiverElementDownload = useRef<PDFExport | null>(null);
+
   // only update signature width when the signature block is visible
   useEffect(() => {
     if (signatureBlockCardRef.current !== null) {
       setCanvasWidth(signatureBlockCardRef.current.clientWidth);
     }
   }, [signatureBlockCardRef]);
-  
+
   // set inital canvas width to null on load
   const [canvasWidth, setCanvasWidth] = useState<number | undefined>();
 
@@ -77,9 +100,9 @@ export default function SignWaiver() {
   useEffect(() => {
     const handleResize = () => {
       if (signatureBlockCardRef.current !== null) {
-      setCanvasWidth(Math.floor(signatureBlockCardRef.current.clientWidth));
-    }
-  };
+        setCanvasWidth(Math.floor(signatureBlockCardRef.current.clientWidth));
+      }
+    };
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
@@ -114,115 +137,135 @@ export default function SignWaiver() {
 
   // handler Funtions
   const handleUpdateSignature = async () => {
-    if (signatureRef.current !== null){
-    const signatureImg = await signatureRef.current.getTrimmedCanvas().toDataURL('image/png');
-    setSignature(signatureImg);
+    if (signatureRef.current !== null) {
+      const signatureImg = await signatureRef.current.getTrimmedCanvas().toDataURL('image/png');
+      setSignature(signatureImg);
     }
   };
   const onSubmit = async () => {
-    const drawnDOM = await drawDOM(pdfWaiverElement.current, {
-      paperSize: 'A4',
-      margin: '1cm',
-      scale: .55,
-    });
-    const signedWaiver = await exportPDF(drawnDOM);
-    console.log(signedWaiver);
+    let drawnDOM;
+    if (pdfWaiverElement.current) {
+      drawnDOM = await drawDOM(pdfWaiverElement.current, {
+        paperSize: 'A4',
+        margin: {
+          top: '1cm',
+          right: '1cm',
+          bottom: '1cm',
+          left: '1cm',
+        },
+        scale: 0.55,
+      })
+    } else {
+      // Handle the case when pdfWaiverElement.current is null or undefined
+      console.log('pdfWaiverElement.current is null or undefined');
+    };
+    if (drawnDOM) {
+      const signedWaiver = await exportPDF(drawnDOM);
+      console.log(signedWaiver);
+    }
+    else {
+      // Handle the case when drawnDOM is null or undefined
+      console.log('drawnDOM is null or undefined');
+    }
     // post cloudinary url to server
     const isDesktop = !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    if (isDesktop) {
+    if (isDesktop && pdfWaiverElementDownload.current) {
       console.log(pdfWaiverElementDownload.current);
       await pdfWaiverElementDownload.current.save();
     }
-    await submitSignedWaiver({
-      signedWaiver,
-      customerId: customer.id,
-    }); 
-    enqueueSnackbar('Waiver Signed');
-    push(paths.waiverConfirmation);
-  };
+  //   await submitSignedWaiver({
+  //     signedWaiver,
+  //     customerId: customer.id,
+  //   });
+  //   enqueueSnackbar('Waiver Signed');
+  //   push(paths.waiverConfirmation);
+  // };
 
   // clear signature on click
-  const handleClearSignature = () => signatureRef.current.clear();
+  const handleClearSignature = () => {
+    if(signatureRef.current){
+      signatureRef.current.clear()
+    };
+  }
 
   return (
 
-      <Container maxWidth={settings.themeStretch ? false : 'lg'}>
-        <CustomBreadcrumbs
-          heading="Sign Waiver"
-          links={[
-            { name: 'Edit Account Members', href: `#` },
-            { name: 'User', href: paths.dashboard.user.list },
-            // { name: !isEdit ? 'New user' : capitalCase('name') },
-          ]}
-        />
-        <PDFExport ref={pdfWaiverElementDownload} paperSize="Letter" imageResolution={300} scale={0.55} margin={10}>
-          <Grid ref={pdfWaiverElement} container spacing={2}>
-            <Grid item xs={12}>
-              <HTMLBlock waiverText={safeHTML} />
-            </Grid>
-            <Grid alignItems="center" container spacing={2} item xs={6}>
-              <Grid item>
-                <Typography variant="h4" component="h4">
-                  Signing for:
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography
-                  alignSelf={'center'}
-                  textAlign="left"
-                  variant="p"
-                  component="p"
-                  sx={{
-                    marginLeft: '1rem',
-                  }}
-                >
-                  {customer?.guardianFirstName} {customer?.guardianLastName}
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid alignItems="center" container item xs={6}>
+    <Container maxWidth={settings.themeStretch ? false : 'lg'}>
+      <CustomBreadcrumbs
+        heading="Sign Waiver"
+        links={[
+          { name: 'Edit Account Members', href: `#` },
+          { name: 'User', href: paths.dashboard.user.list },
+          // { name: !isEdit ? 'New user' : capitalCase('name') },
+        ]}
+      />
+      <PDFExport ref={pdfWaiverElementDownload} paperSize="Letter" imageResolution={300} scale={0.55} margin={10}>
+        <Grid  component="div" ref={pdfWaiverElement} container spacing={2} >
+          <Grid item xs={12}>
+            <HTMLBlock waiverText={safeHTML} />
+          </Grid>
+          <Grid alignItems="center" container spacing={2} item xs={6}>
+            <Grid item>
               <Typography variant="h4" component="h4">
-                Minors:
+                Signing for:
               </Typography>
-
-              {customer?.minors &&
-                customer?.minors.map((minor, i) => (
-                  <Grid item key={minor + i}>
-                    <Typography variant="p" component="p" sx={{ marginLeft: '1rem' }}>
-                      {customer?.minors.length - 1 !== i
-                        ? `${minor.minorFirstName}
-                      ${minor.minorLastName},`
-                        : `${minor.minorFirstName} ${minor.minorLastName}`}
-                    </Typography>
-                  </Grid>
-                ))}
             </Grid>
-            <Grid item xs={12}>
-              <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-                <Card sx={{ border: 1, marginBottom: 3 }} ref={signatureBlockCardRef}>
-                  <SignatureCanvas
-                    id={`signature`}
-                    onEnd={handleUpdateSignature}
-                    ref={signatureRef}
-                    canvasProps={{
-                      width: canvasWidth,
-                      height: 200,
-                    }}
-                  />
-                </Card>
-                <Stack gap={3} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }} sx={{ my: 3 }}>
-                  <Button color="inherit" variant="outlined" onClick={() => handleClearSignature()}>
-                    Clear Signature
-                  </Button>
-                  <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-                    Submit Waiver
-                  </LoadingButton>
-                </Stack>
-              </FormProvider>
+            <Grid item xs={6}>
+              <Typography
+                alignSelf={'center'}
+                textAlign="left"
+                component="p"
+                sx={{
+                  marginLeft: '1rem',
+                }}
+              >
+                {customer?.guardianFirstName} {customer?.guardianLastName}
+              </Typography>
             </Grid>
           </Grid>
-        </PDFExport>
-      </Container>
-      
+          <Grid alignItems="center" container item xs={6}>
+            <Typography variant="h4" component="h4">
+              Minors:
+            </Typography>
+
+            {customer?.minors &&
+              customer?.minors.map((minor, i) => (
+                <Grid item key={minor.id + i}>
+                  <Typography component="p" sx={{ marginLeft: '1rem' }}>
+                    {customer?.minors.length - 1 !== i
+                      ? `${minor.minorFirstName}
+                      ${minor.minorLastName},`
+                      : `${minor.minorFirstName} ${minor.minorLastName}`}
+                  </Typography>
+                </Grid>
+              ))}
+          </Grid>
+          <Grid item xs={12}>
+            <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+              <Card sx={{ border: 1, marginBottom: 3 }} ref={signatureBlockCardRef}>
+                <SignatureCanvas
+                  id={`signature`}//does not recognize id
+                  onEnd={handleUpdateSignature}
+                  ref={signatureRef}
+                  canvasProps={{
+                    width: canvasWidth,
+                    height: 200,
+                  }}
+                />
+              </Card>
+              <Stack gap={3} justifyContent="space-between" direction={{ xs: 'column', sm: 'row' }} sx={{ my: 3 }}>
+                <Button color="inherit" variant="outlined" onClick={() => handleClearSignature()}>
+                  Clear Signature
+                </Button>
+                <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+                  Submit Waiver
+                </LoadingButton>
+              </Stack>
+            </FormProvider>
+          </Grid>
+        </Grid>
+      </PDFExport>
+    </Container>
+
   );
-}
+}}
